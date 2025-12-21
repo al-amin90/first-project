8b0d9ae4bb36b4a8dd1f2b3df19cba369412521b
al-amin90 <ijesun30@gmail.com>
2025-12-21 07:26:17 +0600

    [update] validate using Joi

--- src/app/modules/student/student.model.ts
+++ src/app/modules/student/student.model.ts
@@ -13,23 +13,23 @@ const userNameSchema = new Schema<TUserName>({
     required: [true, 'First is Required'],
     trim: true,
     maxlength: [20, 'Not more then 20'],
-    validate: {
-      validator: function (value: string) {
-        const firstName =
-          value.charAt(0).toUpperCase() + value.slice(1).toLowerCase()
-        return firstName === value
-      },
-      message: '{VALUE} is not in Capitalize formate',
-    },
+    // validate: {
+    //   validator: function (value: string) {
+    //     const firstName =
+    //       value.charAt(0).toUpperCase() + value.slice(1).toLowerCase()
+    //     return firstName === value
+    //   },
+    //   message: '{VALUE} is not in Capitalize formate',
+    // },
   },
   middleName: String,
   lastName: {
     type: String,
     required: [true, 'Last is Required'],
-    validate: {
-      validator: value => validator.isAlpha(value),
-      message: '{VALUE} is not Valid',
-    },
+    // validate: {
+    //   validator: value => validator.isAlpha(value),
+    //   message: '{VALUE} is not Valid',
+    // },
   },
 })
 
@@ -64,10 +64,10 @@ const studentSchema = new Schema<TStudent>({
   email: {
     type: String,
     required: true,
-    validate: {
-      validator: value => validator.isEmail(value),
-      message: '{VALUE} is not Valid Email',
-    },
+    // validate: {
+    //   validator: value => validator.isEmail(value),
+    //   message: '{VALUE} is not Valid Email',
+    // },
   },
   contactNumber: { type: String, required: true },
   avatar: String,
